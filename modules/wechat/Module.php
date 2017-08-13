3ec8f9ce2c3b39ef8f84529bdf9c5323255fee78
xczizz <563900027@qq.com>
2017-08-13 17:09:25 +0800

    微信连接测试

---
+++ modules/wechat/Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: Mr-mao
+ * Date: 2017/8/13
+ * Time: 14:00
+ */
+
+namespace app\modules\wechat;
+
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
---
+++ modules/wechat/controllers/WechatController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * User: Mr-mao
+ * Date: 2017/8/13
+ * Time: 14:08
+ */
+
+
+namespace app\modules\wechat\controllers;
+
+use Yii;
+use yii\web\Response;
+use yii\rest\ActiveController;
+use EasyWeChat\Foundation\Application;
+
+class WechatController extends ActiveController
+{
+    public $modelClass = '';
+    public $options = [];
+
+    public function init()
+    {
+        parent::init();
+        $this->options = [
+            'debug'  => YII_DEBUG,
+            'app_id' => Yii::$app->params['wechat']['id'],
+            'secret' => Yii::$app->params['wechat']['secret'],
+            'token'  => Yii::$app->params['wechat']['token'],
+            'aes_key' => Yii::$app->params['wechat']['aes_key'],
+            'log' => [
+                'level' => 'debug',
+                'file'  => Yii::$app->params['wechat_log_path'], // XXX: 绝对路径！！！！
+            ],
+        ];
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['contentNegotiator']['formats']['text/html'] = Response::FORMAT_JSON;
+
+        return $behaviors;
+    }
+
+    public function actionValid()
+    {
+        $app =  new Application($this->options);
+        $server = $app->server;
+
+        // 测试发送
+        $server->setMessageHandler(function ($message) {
+           return '连接成功';
+        });
+
+        $response = $server->serve();
+        $response->send();
+
+    }
+
+}
