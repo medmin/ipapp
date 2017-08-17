b1cbc448dba7f2c438f7cc4dcce8fcfaa47cb331
medmin <medmin@163.com>
2017-08-17 11:28:03 +0800

    修复process message 的配置，彻底修好了 3

--- modules/wechat/controllers/WechatController.php
+++ modules/wechat/controllers/WechatController.php
@@ -9,7 +9,6 @@
 namespace app\modules\wechat\controllers;
 
 use app\modules\wechat\models\DefaultMenu;
-use app\modules\wechat\models\Options;
 use app\modules\wechat\models\TemplateForm;
 use Yii;
 use EasyWeChat\Foundation\Application;
@@ -21,7 +20,17 @@ class WechatController extends \yii\base\Controller
     public function init()
     {
         parent::init();
-        $this->options = (new Options())->getOptions();
+        $this->options = [
+            'debug'  => YII_DEBUG,
+            'app_id' => Yii::$app->params['wechat']['id'],
+            'secret' => Yii::$app->params['wechat']['secret'],
+            'token'  => Yii::$app->params['wechat']['token'],
+            'aes_key' => Yii::$app->params['wechat']['aes_key'],
+            'log' => [
+                'level' => 'debug',
+                'file'  => Yii::$app->params['wechat_log_path'], // XXX: 绝对路径！！！！
+            ]
+        ];
     }
 
     public function behaviors()
@@ -37,7 +46,7 @@ class WechatController extends \yii\base\Controller
      */
     public function actionValid()
     {
-        $this->options = (new Options())->getOptions();
+
         $app =  new Application($this->options);
         $server = $app->server;
 
@@ -48,7 +57,7 @@ class WechatController extends \yii\base\Controller
 
     public function actionProcessMessage()
     {
-        $this->options = (new Options())->getOptions();
+
         $app =  new Application($this->options);
         $server = $app->server;
 
