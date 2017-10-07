0d0a957b95fdb6ce1ffde6167311e338741784c7
xczizz <563900027@qq.com>
2017-10-08 00:47:06 +0800

    搜索专利修改

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -12,6 +12,7 @@ use yii\web\Controller;
 class BaseController extends Controller
 {
     public $isMicroMessage = false;
+    public $isMobile = false;
 
     /**
      * @param \yii\base\Action $action
@@ -22,6 +23,7 @@ class BaseController extends Controller
             return false;
         }
         $this->isMicroMessage = $this->isMicroMessage();
+        $this->isMobile = $this->isMobile();
         return true;
     }
 
@@ -32,6 +34,33 @@ class BaseController extends Controller
     private function isMicroMessage() {
         if (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
             return true;
+        }
+        return false;
+    }
+
+    /**
+     * 是否移动端访问
+     * @return bool
+     */
+    private function isMobile() {
+        if (isset ($_SERVER['HTTP_X_WAP_PROFILE'])) {
+            return true;
+        }
+        if (isset ($_SERVER['HTTP_VIA'])) {
+            // 找不到为flase,否则为true
+            return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+        }
+        $client_keywords = ['nokia', 'sony', 'ericsson', 'mot', 'samsung', 'htc', 'sgh', 'lg', 'sharp', 'sie-', 'philips', 'panasonic', 'alcatel', 'lenovo', 'iphone', 'ipod', 'blackberry', 'meizu', 'android', 'netfront', 'symbian', 'ucweb', 'windowsce', 'palm', 'operamini', 'operamobi', 'openwave', 'nexusone', 'cldc', 'midp', 'wap', 'mobile'];
+        if (preg_match("/(" . implode('|', $client_keywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+            return true;
+        }
+        // 协议法，因为有可能不准确，放到最后判断
+        if (isset ($_SERVER['HTTP_ACCEPT'])) {
+            // 如果只支持wml并且不支持html那一定是移动设备
+            // 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
+                return true;
+            }
         }
         return false;
     }
